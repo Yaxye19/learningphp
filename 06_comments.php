927069ab5154d95b01cf813bfa305948567d32be
Yaxye19 <yaxyeali12@outlook.com>
2023-01-15 13:25:16 +0000

    explained comments with examples

---
+++ 06_comments.php
@@ -0,0 +1,54 @@
+<?php
+
+// This is a single line comment
+
+$panda = 'Awesome'; // Set panda to awesome.
+
+// Pandas are awesome.
+// It's because they are kind of like cats.
+// Except more fluffy, and they kind of look like
+// how cats look in anime.
+
+
+/* Pandas are awesome.
+   It's because they are kind of like cats.
+   Except more fluffy, and they kind of look like
+   how cats look in anime.
+*/
+$pandas = 'Awesome!';
+
+
+/* Pandas are awesome.
+ * It's because they are kind of like cats.
+ * Except more fluffy, and they kind of look like
+ * how cats look in anime.
+ */
+$pandas = 'Awesome!';
+
+// Set a string.
+$pandas = 'awesome!';
+
+// Set a string.
+$pandas = 'fantastic!';
+
+// Output.
+echo 'Pandas are ' . $pandas;
+
+// Set a string.
+$pandas = 'awesome!';
+
+/*
+// Set a string.
+$pandas = 'fantastic!';
+*/
+
+// Output.
+echo 'Pandas are ' . $pandas;
+
+
+/**
+ * Description of pandas.
+ *
+ * @var string
+ */
+$pandas = 'awesome!';
