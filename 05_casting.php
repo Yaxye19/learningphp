ee9912f8f6499aea585f37a5b7c3bb7db10c259c
Yaxye19 <yaxyeali12@outlook.com>
2023-01-15 13:19:49 +0000

    explained casting with examples

---
+++ 05_casting.php
@@ -0,0 +1,26 @@
+<?php
+// Set a string value
+$panda = '3';
+
+// Cast to a string
+$panda = (int) $panda;
+
+//var_dump($panda);
+
+// Set a float value.
+$panda2 = 3.4567;
+
+// Cast to string.
+$panda2 = (string) $panda2;
+
+// Dump result.
+//var_dump($panda2);
+
+// Set a string value.
+$panda = 'Lushui';
+
+// Cast to array.
+$panda = (array) $panda;
+
+// Dump result.
+var_dump($panda);
