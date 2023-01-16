1bb4824ed928202f3b9c9a24bfad46c00359db1c
Yaxye19 <yaxyeali12@outlook.com>
2023-01-16 17:19:26 +0000

    explained what includes is

---
+++ 11_includes.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * It would be a shame if you had to keep writing the same code over and over again, wouldn't it? 
+ * I mean, it was *fun to write, 
+ * but it would slowly wear away at you. 
+ * Let's imagine that you have the following function.
+ 
+ *   function welcome($name) {
+ *     return "Welcome, {$name}!";
+ *   }
+ * 
+ *  We can resuse this function by including it in other files
+ * see the welcome.php file and program.php file
+ * 
+ * 
+ */
