8f5b3b3878f119d6fdb206003af03a4c86e854ef
Yaxye19 <yaxyeali12@outlook.com>
2023-01-17 01:39:09 +0000

    explained scope with examples

--- 14_scope.php
+++ 14_scope.php
@@ -1,3 +1,32 @@
 <?php
 
-echo 'j';
+// class Panda {
+
+//     public $coat = 'fluffy';
+
+//     public function getCoat() {
+//         return $this->coat;
+//     }
+// }
+
+class Panda {
+
+    // Declare a private property...
+    private $coat = 'fluffy';
+
+    //... and a public function
+    private function getCoat() {
+       return $this->coat;
+    }
+
+    public function notReallyGetCoat() {
+        return $this->getCoat();
+    }
+
+}
+
+    //make new instance of panda
+    $panda = new Panda;
+
+    //Try to echo the coat property.
+    echo $panda->notReallygetCoat();
